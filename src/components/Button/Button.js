97e5519ebc677d0ae6b40b31386f5a3f4f1b5159
kgy8987 <rlarldud8987@naver.com>
2023-09-19 16:38:21 +0900

    test

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './Button.scss';
 
-const ButtonCompornent = (props) => {
+const Button = (props) => {
+    const {
+        type,
+        value,
+        name,
+        size,
+        scale,
+        onclick,
+        classname =''
+    } = props;
 return (
-<button></button>;
-)};
+    <button
+        type={type}
+        value={value}
+        name={name}        size=
+        scale={scale}
+        onclick={onclick}
+        classname={classname}
+    />
+    );
+};
 
 export default Button;
