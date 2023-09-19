44c80a82260b4c157e01e45d9f49271bb9804977
Minjae Park <jayy.intheworld@gmail.com>
2023-09-19 16:40:27 +0900

    [etc] branch 초기세팅 완료

---
+++ src/components/Input/Input.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Input from "./Input.scss";
+
+const Input = () => {
+
+  return(
+    <div>
+      <input>yeah!</input>
+    </div>
+  )
+}
+
+export default Input;
