09cea7f2378d95c6950a3713a95bf671e9d57a4b
Leechengwon <leecwee8497@gmail.com>
2023-09-20 10:57:31 +0900

    Nav 컴포넌트 레이아웃 작업 중

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,39 +1,51 @@
 import React from 'react';
 import './Nav.scss';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Nav = () => {
+  const navifate = useNavigate();
   return (
     <nav className="nav">
       <p className="event">
         <b>[신규혜택]</b> 1만원 쿠폰팩 증정
       </p>
       <header>
-        <div>
-          <span className="mainTitle">widely</span>
-          <span>최고의 가성비가 아니면 팔지않습니다.</span>
-        </div>
-        <div>
-          <span>가입/로그인</span>
-          <span>고객센터</span>
-          <span>공지사항</span>
+        <div className="inner-wrap">
+          <div>
+            <Link to="/main" className="mainTitle">
+              widely
+            </Link>
+            <span>최고의 가성비가 아니면 팔지않습니다.</span>
+          </div>
+          <div>
+            <Link className="loginLink" to="/login">
+              가입/로그인
+            </Link>
+            <span>고객센터</span>
+            <span>공지사항</span>
+          </div>
         </div>
       </header>
       <div className="viewList">
-        <div className="kateGoRie">
-          <i class="fa-solid fa-bars" />
-          <span>카테고리</span>
-        </div>
-        <div className="tabWrapper">
-          <span>홈</span>
-          <span>신제품</span>
-          <span>베스트</span>
-          <span>와이들리 LAB</span>
-        </div>
-        <div className="inputI">
-          <input type="text" />
-          <i class="fa-solid fa-magnifying-glass" />
-          <i class="fa-regular fa-heart" />
-          <i class="fa-solid fa-bag-shopping" />
+        <div className="inner-wrap">
+          <div className="kateGoRie">
+            <i className="fa-solid fa-bars" />
+            <span>카테고리</span>
+          </div>
+          <div className="tabWrapper">
+            <span>홈</span>
+            <span>신제품</span>
+            <span>베스트</span>
+            <span>와이들리 LAB</span>
+          </div>
+          <div className="inputI">
+            <div className="glasses">
+              <input type="text" />
+              <i className="fa-solid fa-magnifying-glass" />
+            </div>
+            <i className="fa-regular fa-heart" />
+            <i className="fa-solid fa-bag-shopping" />
+          </div>
         </div>
       </div>
     </nav>
