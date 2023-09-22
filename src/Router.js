a0123852e5e807bfea57935756546a62b162e0ae
kgy8987 <rlarldud8987@naver.com>
2023-09-22 18:48:24 +0900

    Button 컴포넌트 구현(2차)

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
+import Basket from './pages/Basket/Basket';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/basket" element={<Basket />} />
       </Routes>
     </BrowserRouter>
   );
