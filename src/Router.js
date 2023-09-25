9e9da5e4e2362a2abcb11cd99483d8095ff0e19f
Minjae Park <hogooma1@gmail.com>
2023-09-25 21:29:34 +0900

    Revert "Component/button basket(작업이슈, 브랜치명변경)"

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
-import Basket from './pages/Basket/Basket';
 
 const Router = () => {
   return (
@@ -10,7 +9,6 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/main" element={<Main />} />
-        <Route path="/basket" element={<Basket />} />
       </Routes>
     </BrowserRouter>
   );
