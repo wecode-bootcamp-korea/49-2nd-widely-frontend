f6a9f7c05779f9e97f0741ecbb8f0a2ca974dd53
Minjae Park <jayy.intheworld@gmail.com>
2023-09-26 18:30:09 +0900

    button/basket 재전송

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Button.scss';
 
-const ButtonCompornent = (props) => {
-return (
-<button></button>;
-)};
-
-export default Button;
-ㅎ
+const Button = (props) => {
+  const {
+    type,
+    value,
+    name,
+    size,
+    shape,
+    disabled,
+    onClick,
+    className = 'btn',
+  } = props;
+  return (
+    <button
+      type={type}
+      value={value}
+      name={name}
+      size={size}
+      shape={shape}
+      disabled={disabled}
+      onClick={onClick}
+      className={className}
+    />
+  );
+};
