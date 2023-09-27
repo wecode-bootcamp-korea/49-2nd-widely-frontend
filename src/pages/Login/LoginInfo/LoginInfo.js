530673870589045d28e499b0500e38f6f43f57e8
Minjae Park <jayy.intheworld@gmail.com>
2023-09-27 15:02:29 +0900

    [feat] 로그인 페이지 UI 진행중

---
+++ src/pages/Login/LoginInfo/LoginInfo.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Input from '../../../components/Input/Input';
+import './LoginInfo.scss';
+
+const LoginInfo = () => {
+  return (
+    <form>
+      <fieldset>
+        <legend>로그인</legend>
+        <div className="loginInput">
+          <Input
+            type="email"
+            name="email"
+            placeholder="이메일을 입력해주세요"
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력해주세요"
+          />
+        </div>
+        <div className="loginRadio">
+          <Input type="checkbox" />
+          아이디 저장
+          <Input type="checkbox" />
+          보안접속
+        </div>
+        <div className="loginButton">
+          <button>로그인</button>
+          <button>회원가입</button>
+        </div>
+        <ul className="search">
+          <li>회원가입</li>
+          <li>비밀번호 찾기</li>
+        </ul>
+      </fieldset>
+    </form>
+  );
+};
+
+export default LoginInfo;
