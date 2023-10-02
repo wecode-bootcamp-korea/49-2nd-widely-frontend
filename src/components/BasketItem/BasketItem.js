d0a340df3f8c114488bd9da54f5b10a5d9861cc3
kgy8987 <rlarldud8987@naver.com>
2023-10-02 15:02:18 +0900

    Button수정

---
+++ src/components/BasketItem/BasketItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './BasketItem.scss';
+
+const BasketItem = () => {
+  return <div />;
+};
+
+export default BasketItem;
--- src/pages/Basket/Basket.js
+++ src/pages/Basket/Basket.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './Basket.scss';
-import Button from '../../components/Button/Button';
+import BasketPrice from '../../components/BasketPrice/BasketPrice';
 
 const Basket = () => {
   return (
     <div className="cartMain">
-      <div className="topContainer">
-        <header className="test1">테스트구역</header>
-        <div>테스트구역2</div>
-      </div>
-      <div className="buttomContainer">
+      <div className="Container">
         <div className="btSection">
           <div className="btwrap">
             <h2>장바구니</h2>
@@ -24,31 +20,7 @@ const Basket = () => {
                 </div>
                 <div className="infoWrap">
                   <p className="messageWed">장바구니에 담긴 제품이 없습니다</p>
-                  <Button className="btn" size="smallB" />
-                  <div className="orderInfo">
-                    <div className="bsTotalBox">
-                      <div className="bsTotalPrice">
-                        <span>합계</span>
-                        <span>원</span>
-                      </div>
-                      <div className="bsDcPrice">
-                        <span>상품할인금액</span>
-                        <span>-원</span>
-                      </div>
-                      <div className="bsDeliveryPrice">
-                        <span>배송비</span>
-                        <span>+원</span>
-                      </div>
-                      <div className="bsTotal">
-                        <span>결제예정금액</span>
-                        <span>0원</span>
-                      </div>
-                    </div>
-                    <button className="bsChBtn">제품을 담아주세요</button>
-                    <span className="couponAlram">
-                      ˑ 쿠폰과 적립금은 결제 페이지에서 적용할 수 있어요
-                    </span>
-                  </div>
+                  <BasketPrice />
                 </div>
               </div>
             </div>
